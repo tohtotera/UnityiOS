2103823165d291bb74b2400921c9a78ef6c7511e
weider <tohtotera@ab.auone-net.jp>
2015-11-13 21:42:14 +0900

    no message

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -1,53 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Hole : MonoBehaviour 
-{
-	bool fallIn;
+public class Hole : MonoBehaviour {
 
-	// どのボールを吸い寄せるかをタグで指定
-	public string activeTag;
+    bool fallIn;
+    public string activeTag;
 
-        // ボールが入っているかを返す
-	public bool IsFallIn()
-	{
-		return fallIn;
+    public bool isFallIn()
+    {
+        return fallIn;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == activeTag)
+        {
+            fallIn = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == activeTag)
+        {
+            fallIn = false;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+        Vector3 direction = transform.position - other.gameObject.transform.position;
+        direction.Normalize();
+
+        if (other.gameObject.tag == activeTag)
+        {
+            r.velocity *= 0.9f;
+            r.AddForce(direction * r.mass * 20.0f);
+        }
+        else
+        {
+            r.AddForce(-direction * r.mass * 80.0f);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+	
 	}
 	
-	void OnTriggerEnter (Collider other)
-	{
-		if(other.gameObject.tag == activeTag)
-		{
-			fallIn = true;
-		}
-	}
+	// Update is called once per frame
+	void Update () {
 	
-	void OnTriggerExit (Collider other)
-	{
-		if(other.gameObject.tag == activeTag)
-		{
-			fallIn = false;
-		}
-	}
-
-	void OnTriggerStay (Collider other)
-	{
-		// コライダに触れているオブジェクトのRigidbodyコンポーネントを取得 
-		Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
-		
-                // ボールがどの方向にあるかを計算
-		Vector3 direction = transform.position - other.gameObject.transform.position;
-		direction.Normalize();
-		
-                // タグに応じてボールに力を加える
-		if (other.gameObject.tag == activeTag)
-		{
-                        // 中心地点でボールを止めるため速度を減速させる 
-			r.velocity *= 0.9f;
-
-			r.AddForce(direction * r.mass * 20.0f);
-		} else {
-			r.AddForce(- direction * r.mass * 80.0f);
-		}
 	}
 }
